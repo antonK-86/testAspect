b82608ae09a8b1afc9ef448afa9389365a37250e
antonK-86 <ank86@inbox.ru>
2020-07-15 14:12:45 +0300

    Updates

---
+++ precache-manifest.f830c6df8e124b15294549850cde55b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6db8eb8c67cc5e36e30279ec9ba6c41",
+    "url": "/testAspect/index.html"
+  },
+  {
+    "revision": "57f79aac6c87c4ae5755",
+    "url": "/testAspect/static/css/main.a69e278a.chunk.css"
+  },
+  {
+    "revision": "7142648776b990d3af53",
+    "url": "/testAspect/static/js/2.96f2cabe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/testAspect/static/js/2.96f2cabe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57f79aac6c87c4ae5755",
+    "url": "/testAspect/static/js/main.9abf3a5c.chunk.js"
+  },
+  {
+    "revision": "18482287c4706b4ff58a",
+    "url": "/testAspect/static/js/runtime-main.c29f1db5.js"
+  }
+]);
